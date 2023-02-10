90edbab41ab3dae5737d9abf17fc925e10ba849a
manvithshetty507 <manvithshetty18@gmail.com>
2023-02-11 00:49:58 +0530

    third commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -70,7 +70,7 @@ public class Pizza {
 
         if(addBag) bill += "Paperbag Added: "+20+"\n";
 
-        bill += "Total Price: " + getPrice()+"\n"git ;
+        bill += "Total Price: " + getPrice()+"\n" ;
 
         return this.bill;
     }
